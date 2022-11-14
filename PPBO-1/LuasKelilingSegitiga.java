ed5bdc4a1486b4c1d2087bc5f4922a7ac56fb3d3
Alfurqon02 <89855750+Alfurqon02@users.noreply.github.com>
2022-11-14 07:58:13 +0700

    Create LuasKelilingSegitiga.java

---
+++ PPBO-1/LuasKelilingSegitiga.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LuasKelilingSegitiga {
+    int a, b, c;
+
+    // Dokumentasi constructor Java w3schools.com/java/java_constructors.asp
+    public LuasKelilingSegitiga(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double hitungLuas() {
+        // Silakan lengkapi method ini untuk menghitung luas
+    }
+
+    public double hitungKeliling() {
+        // Silakan lengkapi method ini untuk menghitung keliling
+    }
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        // Silakan lengkapi 3 potongan dibawah untuk menangkap input user
+        int a = // Potongan 1
+        int b = // Potongan 2
+        int c = // Potongan 3
+        LuasKelilingSegitiga lksegitiga = new LuasKelilingSegitiga(a, b, c);
+
+        // Silakan lengkapi bagian ini untuk mencetak hasil
+
+        myScanner.close();
+    }
+}
