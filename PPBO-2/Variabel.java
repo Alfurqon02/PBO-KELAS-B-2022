3f6b1caf68b5a1149f88b9d64f42d6361e8ae1ca
Alfurqon02 <89855750+Alfurqon02@users.noreply.github.com>
2022-11-14 08:18:57 +0700

    Create Variabel.java

---
+++ PPBO-2/Variabel.java
@@ -0,0 +1,13 @@
+public class Variabel {
+    public static void main(String[] args) {
+        // Deklarasikan variabel di sini, sesuai dengan tipe datanya
+        int x = 123;
+        float y = 243.584F;
+        String z = "Hello";
+
+        // Cetak variabel di sini
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
