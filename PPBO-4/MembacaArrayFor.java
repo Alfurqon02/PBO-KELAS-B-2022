10c61a8f59fb6525af18908ef28604db14cb0b52
Alfurqon02 <89855750+Alfurqon02@users.noreply.github.com>
2022-11-15 16:44:44 +0700

    Create MembacaArrayFor.java

---
+++ PPBO-4/MembacaArrayFor.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArrayPBO4 {
+    public static void main(String[] args) {
+        int arr[][] = {
+                { 1, 2, 3, 4, 5 },     
+                { 6, 7, 8, 9, 0 },     
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 24, 25 },
+                { 26, 27, 28, 29, 30 } 
+        };
+
+        Scanner scan = new Scanner(System.in);
+        int c1 = scan.nextInt();
+        int c2 = scan.nextInt();
+
+        int arr1[] = arr[c1];
+        int arr2[] = arr[c2];
+        
+          //Lengkapi code  mulai dari sini. Wajib menggunakan for 
+        for (int n = 0, m = arr2.length - 1; n < arr1.length || m >= 0;){
+            System.out.print("--" + arr1[n++] + "|" + arr2[m--]);
+        }
+            System.out.print("--");  
+        scan.close();
+    }
+}
